e1d958ee2c2b541f2cd9bc3dd3d693c45bb4d349
Nataniel <natmartin31@gmail.com>
2022-11-25 10:02:39 +0100

    Creation of project

---
+++ Overseerr/OverseerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverseerrApp.swift
+//  Overseerr
+//
+//  Created by Nataniel Martin on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct OverseerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
